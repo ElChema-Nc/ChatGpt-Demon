842df1300c1768effc1aef8a75518538704cdbd5
El Chema <106692495+GyutaroNc@users.noreply.github.com>
2023-05-02 05:10:47 +0200

    Update Generator.tsx

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -227,7 +227,7 @@ export default () => {
         when={!loading()}
         fallback={() => (
           <div class="gen-cb-wrapper">
-            <span>AI is thinking...</span>
+            <span>Estoy pensando...</span>
             <div class="gen-cb-stop" onClick={stopStreamFetch}>Stop</div>
           </div>
         )}
@@ -237,7 +237,7 @@ export default () => {
             ref={inputRef!}
             disabled={systemRoleEditing()}
             onKeyDown={handleKeydown}
-            placeholder="Enter something..."
+            placeholder="ingresa tu pregunta..."
             autocomplete="off"
             autofocus
             onInput={() => {
