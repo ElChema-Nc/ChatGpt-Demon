0bed75203c8a40e832436732ffcf07a3d853a844
El Chema <106692495+GyutaroNc@users.noreply.github.com>
2023-05-01 21:39:58 -0600

    Add files via upload

--- hack/docker-env-replace.sh
+++ hack/docker-env-replace.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-# Your API Key for OpenAI
+# Su clave API para OpenAI
 openai_api_key=$OPENAI_API_KEY
-# Provide proxy for OpenAI API. e.g. http://127.0.0.1:7890
+# Proporcione un proxy para la API de OpenAI. e.g. http://127.0.0.1:7890
 https_proxy=$HTTPS_PROXY
-# Custom base url for OpenAI API. default: https://api.openai.com
+# URL base personalizada para la API de OpenAI. por defecto: https://api.openai.com
 openai_api_base_url=$OPENAI_API_BASE_URL
-# Inject analytics or other scripts before </head> of the page
+# Inyectar análisis u otras secuencias de comandos antes de </head> de la página
 head_scripts=$HEAD_SCRIPTS
-# Secret string for the project. Use for generating signatures for API calls
+# Cadena secreta para el proyecto. Uso para generar firmas para llamadas API
 public_secret_key=$PUBLIC_SECRET_KEY
-# Set password for site, support multiple password separated by comma. If not set, site will be public
+# Establezca una contraseña para el sitio, admita varias contraseñas separadas por comas. Si no se establece, el sitio será público
 site_password=$SITE_PASSWORD
-# ID of the model to use. https://platform.openai.com/docs/api-reference/models/list
+# ID del modelo a utilizar. https://platform.openai.com/docs/api-reference/models/list
 openai_api_model=$OPENAI_API_MODEL
 
 for file in $(find ./dist -type f -name "*.mjs"); do
