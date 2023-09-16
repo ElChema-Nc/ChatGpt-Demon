01c67a253ba1d28dd96b339e4972cf08a39872d3
Chema <106692495+ElChema-Nc@users.noreply.github.com>
2023-09-16 22:17:09 +0200

    Add files via upload

--- src/components/SystemRoleSettings.tsx
+++ src/components/SystemRoleSettings.tsx
@@ -55,7 +55,7 @@ export default (props: Props) => {
             <IconEnv />
             <span>Rol del sistema:</span>
           </div>
-          <p class="my-2 leading-normal text-sm op-50 dark:op-60">Gently instruct the assistant and set the behavior of the assistant.</p>
+          <p class="my-2 leading-normal text-sm op-50 dark:op-60">Instruya suavemente al asistente y establezca su comportamiento.</p>
           <div>
             <textarea
               ref={systemInputRef!}
@@ -68,7 +68,7 @@ export default (props: Props) => {
           </div>
           <div class="w-full fi fb">
             <button onClick={handleButtonClick} gen-slate-btn>
-              Set
+              Colocar
             </button>
             <div class="w-full ml-2">
               <SettingsSlider
